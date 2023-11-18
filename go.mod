335ba4942ab30925b70af047c26eb77fa923c4b4
Дмитрий Коваль <koval_minimum@icloud.com>
2023-11-18 12:21:09 +0300

    rework createPayload()

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module jwt_server
 
 go 1.21.0
-
-require github.com/julienschmidt/httprouter v1.3.0
--- main.go
+++ main.go
@@ -1,15 +1,31 @@
+// *Генератор JWT*
+// Для получения сгенерированного токена необходимо соблюдать строгий формат данных json
+// Обычно в body передаётся "полезная нагрузка" в формате json
+// Например,
+// {
+// 	"login":"apple",
+// 	"pass":"banana",
+// 	"mail": "juice"
+// }
+// При соблюдении данного формата, в response возвращается полностью сгенерированный JWT
+// Не обязательно передавать данные, указанные в примере выше
+// Вы так же можете описывать необходимую для Вас полезную нагрузку, добавив, например, время создания токена
+// {
+// 	"time", "2023-11-11T16:00:00:000"
+// }
+// Attemption: при увеличении данных в полезной нагрузке, увеличивается и время создания JWT
+
 package main
 
 import (
+	"crypto/hmac"
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 type header struct {
@@ -17,90 +33,68 @@ type header struct {
 	Typ string `json:"type"`
 }
 
-type payload struct {
-	Name string `json:"name"`
-	Pass string `json:"pass"`
-	Mail string `json:"mail"`
+type Payload struct {
+	pl map[string]string
 }
 
-func (pa *payload) createPayload(n, p, m string) {
-	if len(n) < 3 || len(p) < 3 || len(m) < 3 {
-		log.Fatal("Не корректные данные")
+func (pa *Payload) CreatePayload(body string) {
+	if err := json.Unmarshal([]byte(body), &pa.pl); err != nil {
+		fmt.Printf("Ошибка при конвертации body в json: %v", err)
 	}
-	pa.Name = n
-	pa.Pass = p
-	pa.Mail = m
+	log.Printf("PAYLOAD IN MAP: %+v", pa.pl)
 }
 
 func main() {
-	router := httprouter.New()
-	router.POST("/registration", Index)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	var pl Payload
+	pl.CreatePayload(`{"login":"apple", "pass":"banana", "mail": "koval_minimu@icloud.com"}`)
+	log.Printf("%+v\n", CreateToken(pl))
 }
 
-func createSecret() string {
-	key := make([]byte, 5)
+// Возвращает 'string' случайный ключ для шифрования из 10 символов
+func createSecret() []byte {
+	key := make([]byte, 64)
 
 	_, err := rand.Read(key)
 	if err != nil {
 		log.Fatalf("Не удалось создать ключи шифрования: %v\n", err)
 	}
-	return fmt.Sprintf("%x", key)
+
+	return key
 }
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	var pl payload
-	err := json.NewDecoder(r.Body).Decode(&pl)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	token := createToken(pl)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	jsonResp, err := json.Marshal(token)
-	if err != nil {
-		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
-	}
-	w.Write(jsonResp)
+type token struct {
+	Token  string `json:"token"`
+	Secret string `json:"secret"`
 }
 
-func createToken(pl payload) string {
+func CreateToken(pl Payload) token {
 	h := header{
-		Alg: "SHA256",
+		Alg: "SHA512",
 		Typ: "JWT",
 	}
 
-	pl.createPayload("nekogan", "pass", "mail")
-
-	fmt.Printf("Конечный ключ шифрования: %s\n", createSecret())
+	key := createSecret()
 
 	hb, err := json.Marshal(h)
 	if err != nil {
 		log.Fatalf("Конвертация header в json: %v\n", err)
 	}
 
-	fmt.Printf("JSON header: %v\n", string(hb))
-
-	pb, err := json.Marshal(pl)
+	pb, err := json.Marshal(pl.pl)
 	if err != nil {
-		log.Fatalf("Конвертация header в json: %v\n", err)
+		log.Fatalf("Конвертация payload в json: %v\n", err)
 	}
 
-	fmt.Printf("JSON payload: %v\n", string(pb))
+	unsignedToken := base64.RawStdEncoding.EncodeToString(hb) + "." + base64.RawStdEncoding.EncodeToString(pb)
 
-	unsignedToken := base64.StdEncoding.EncodeToString(hb) + "." + base64.StdEncoding.EncodeToString(pb)
-
-	fmt.Printf("unsignedToken: %v\n", unsignedToken)
-
-	sha := sha256.New()
+	sha := hmac.New(sha256.New, key)
 	sha.Write([]byte(unsignedToken))
-	bs := sha.Sum(nil)
+	sh := hex.EncodeToString(sha.Sum(nil))
 
-	token := unsignedToken + "." + fmt.Sprintf("%x", bs)
+	tokenStr := unsignedToken + "." + string(sh)
 
-	log.Printf("TOKEN: %v\n", token)
-
-	return token
+	return token{
+		Token:  tokenStr,
+		Secret: base64.RawStdEncoding.EncodeToString(key),
+	}
 }
